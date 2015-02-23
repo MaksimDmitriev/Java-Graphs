4fa102a4367ff413dd52e0f958e9e5bf32418f10
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2015-02-23 13:34:31 +0300

    Remove a doubling main from Vertex.java

--- src/algo/graphs/Graph.java
+++ src/algo/graphs/Graph.java
@@ -204,19 +204,4 @@ public class Graph {
 		// TODO Auto-generated method stub
 
 	}
-
-	public static void main(String[] args) {
-		Graph G = new Graph();
-		G.addEdge("A", "B");
-		G.addEdge("A", "C");
-		G.addEdge("C", "D");
-		G.addEdge("D", "E");
-		G.addEdge("D", "G");
-		G.addEdge("E", "G");
-		G.addVertex("H");
-		// print out G
-		System.out.println(G);
-
-		G.bfs(G.myVertices.get("D"));
-	}
 }
